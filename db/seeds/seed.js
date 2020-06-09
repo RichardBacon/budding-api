659aaad2bd062afb5cfc53c5c7815e5d9e52b37e
Richard Bacon <richardjohnbacon@gmail.com>
2020-06-09 12:22:31 +0100

    add plants and snapshots to seed file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,10 +1,20 @@
-const { userData } = require('../data/index.js');
+const { userData, plantData, snapshotData } = require('../data/index.js');
+
+const { formatDates } = require('../utils/utils');
 
 exports.seed = (knex) => {
   return knex.migrate
     .rollback()
     .then(() => knex.migrate.latest())
     .then(() => {
-      return knex('users').insert(userData).returning('*');
+      return knex('users').insert(userData);
+    })
+    .then(() => {
+      const formattedPlantData = formatDates(plantData);
+      return knex('plants').insert(formattedPlantData);
+    })
+    .then(() => {
+      const formattedSnapshotData = formatDates(snapshotData);
+      return knex('snapshots').insert(formattedSnapshotData);
     });
 };
