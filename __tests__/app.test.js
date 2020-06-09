f104c52cf5fc84e7b7b843fd9c1097a9ff5cd361
joao-mak <34193957+joao-mak@users.noreply.github.com>
2020-06-09 15:26:55 +0100

    tested and implemented getUsers

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,3 +19,49 @@ describe('/', () => {
     });
   });
 });
+
+describe('/users', () => {
+  test('status:405 - invalid method - responds with msg: "method not allowed"', () => {
+    const invalidMethods = ['patch', 'put', 'delete'];
+    const requests = invalidMethods.map((method) => {
+      return request(app)
+        [method]('/api/users')
+        .expect(405)
+        .then(({ body: { msg } }) => {
+          expect(msg).toBe('method not allowed');
+        });
+    });
+
+    return Promise.all(requests);
+  });
+
+  describe('GET', () => {
+    test('status:200 - responds with array of user objects', () => {
+      return request(app)
+        .get('/api/users')
+        .expect(200)
+        .then(({ body: { users } }) => {
+          expect(Array.isArray(users)).toBe(true);
+          expect(users.length).toBe(3);
+        });
+    });
+
+    test('status:200 - each user object has required keys', () => {
+      return request(app)
+        .get('/api/users')
+        .expect(200)
+        .then(({ body: { users } }) => {
+          users.forEach((user) => {
+            expect(user).toContainAllKeys([
+              'user_id',
+              'username',
+              'name',
+              'avatar_url',
+              'email',
+              'password',
+            ]);
+          });
+        });
+    });
+  });
+});
