2e4b335a739bed547ee92cc9d80e1ff9dbae3046
Richard Bacon <richardjohnbacon@gmail.com>
2020-06-10 11:13:51 +0100

    test and implement GET /api/users/:user_id/plants - each plant object has required keys

--- __tests__/plants.test.js
+++ __tests__/plants.test.js
@@ -18,5 +18,26 @@ describe('/api/users/:user_id/plants', () => {
           expect(plants.length).toBe(2);
         });
     });
+
+    test('status:200 - each plant object has required keys', () => {
+      return request(app)
+        .get('/api/users/1/plants')
+        .expect(200)
+        .then(({ body: { plants } }) => {
+          plants.forEach((plant) => {
+            expect(plant).toContainAllKeys([
+              'plant_id',
+              'plant_name',
+              'user_id',
+              'type',
+              'soil',
+              'directSunlight',
+              'inside',
+              'wateringFreq',
+              'created_at',
+            ]);
+          });
+        });
+    });
   });
 });
