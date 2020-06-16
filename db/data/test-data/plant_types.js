5f24e626768b0bce53746f91bce57f70523cf547
Richard Bacon <richardjohnbacon@gmail.com>
2020-06-16 16:37:53 +0100

    add more plant types

--- db/data/test-data/plant_types.js
+++ db/data/test-data/plant_types.js
@@ -1,8 +1,20 @@
 module.exports = [
   {
-    plant_type: 'indoor',
+    plant_type: 'vegetable',
   },
   {
-    plant_type: 'vegetable',
+    plant_type: 'fruit',
+  },
+  {
+    plant_type: 'herb',
+  },
+  {
+    plant_type: 'houseplant',
+  },
+  {
+    plant_type: 'garden',
+  },
+  {
+    plant_type: 'succulent',
   },
 ];
