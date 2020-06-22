311f340faee49409d8ce8713c8257b75ca10a155
ZachPinfold <Zach@tibbi.co.uk>
2020-06-22 22:18:02 +0100

    final changes to db

--- db/data/production-data/snapshots.js
+++ db/data/production-data/snapshots.js
@@ -1,10 +1,87 @@
 module.exports = [
   {
     plant_id: 1,
-    plant_uri:
-      'https://cdn.discordapp.com/attachments/718422373522735155/722409976949375046/image0.jpg',
+    plant_uri: 'https://i.ibb.co/RNnrSnT/1.jpg',
 
     height: 4,
     created_at: '2020-04-23T16:52:52+00:00',
+  },
+  {
+    plant_id: 1,
+    plant_uri: 'https://i.ibb.co/xSBzm8R/2.jpg',
+
+    height: 6,
+    created_at: '2020-05-10T16:52:52+00:00',
+  },
+  {
+    plant_id: 1,
+    plant_uri: 'https://i.ibb.co/nDLZV12/3.jpg',
+
+    height: 10,
+    created_at: '2020-05-18T16:52:52+00:00',
+  },
+  {
+    plant_id: 1,
+    plant_uri: 'https://i.ibb.co/vJW3jGr/4.jpg',
+
+    height: 14,
+    created_at: '2020-05-27T16:52:52+00:00',
+  },
+  {
+    plant_id: 1,
+    plant_uri: 'https://i.ibb.co/PM8nr0V/5.jpg',
+
+    height: 18,
+    created_at: '2020-06-09T16:52:52+00:00',
+  },
+  {
+    plant_id: 1,
+    plant_uri: 'https://i.ibb.co/1r6p6jZ/6.jpg',
+
+    height: 22,
+    created_at: '2020-06-22T16:52:52+00:00',
+  },
+  {
+    plant_id: 2,
+    plant_uri: 'https://i.ibb.co/z6ZtDpM/1.jpg',
+
+    height: 6,
+    created_at: '2020-06-16T16:52:52+00:00',
+  },
+  {
+    plant_id: 3,
+    plant_uri: 'https://i.ibb.co/Yk0JM8n/1.jpg',
+
+    height: 27,
+    created_at: '2020-06-20T16:52:52+00:00',
+  },
+  {
+    plant_id: 4,
+    plant_uri: 'https://i.ibb.co/M152knS/1.jpg',
+
+    height: 17,
+    created_at: '2020-06-21T16:52:52+00:00',
+  },
+
+  {
+    plant_id: 5,
+    plant_uri: 'https://i.ibb.co/T1mm7gR/3.jpg',
+
+    height: 16,
+    created_at: '2020-03-22T20:34:22+00:00',
+  },
+  {
+    plant_id: 5,
+    plant_uri: 'https://i.ibb.co/hsd7PVn/5.jpg',
+
+    height: 26,
+    created_at: '2020-05-20T20:34:22+00:00',
+  },
+  {
+    plant_id: 5,
+    plant_uri: 'https://i.ibb.co/x3ZXM8H/4.jpg',
+
+    height: 38,
+    created_at: '2020-06-23T07:34:22+00:00',
   },
 ];
