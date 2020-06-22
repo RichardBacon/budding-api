47b4dea7f415ff3b18704e311a94cbae3af11a4b
ZachPinfold <Zach@tibbi.co.uk>
2020-06-22 19:28:32 +0100

    new data for testing on the front-end

--- db/data/production-data/snapshots.js
+++ db/data/production-data/snapshots.js
@@ -7,44 +7,4 @@ module.exports = [
     height: 4,
     created_at: '2020-04-23T16:52:52+00:00',
   },
-  {
-    plant_id: 1,
-    plant_uri: '',
-
-    height: 7,
-    created_at: '2020-05-10T16:52:52+00:00',
-  },
-  {
-    plant_id: 1,
-    plant_uri: '',
-
-    height: 10,
-    created_at: '2020-05-18T16:52:52+00:00',
-  },
-  {
-    plant_id: 1,
-    plant_uri: '',
-
-    height: 16,
-    created_at: '2020-05-27T16:52:52+00:00',
-  },
-  {
-    plant_id: 1,
-    plant_uri: '',
-
-    height: 19,
-    created_at: '2020-06-09T16:52:52+00:00',
-  },
-  {
-    plant_id: 1,
-    plant_uri: '',
-    height: 24,
-    created_at: '2020-06-22T16:52:52+00:00',
-  },
-  {
-    plant_id: 2,
-    plant_uri: '',
-    height: 24,
-    created_at: '2020-06-16T16:52:52+00:00',
-  },
 ];
